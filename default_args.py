2fb8115e6503a89c441514b3d1c2e749400ab6ed
bhumivegad <110536859+bhumivegad@users.noreply.github.com>
2023-03-14 17:40:05 +0530

    Add files via upload

---
+++ default_args.py
@@ -0,0 +1,10 @@
+# default argument
+
+def main(a=20 , b=20):
+    print(a+b)
+    
+main(10,20)
+main()
+     
+#30
+#40
---
+++ formal_actual.py
@@ -0,0 +1,11 @@
+
+def sum(a,b):
+    c = a + b          # a and b formal arguments
+    print(c)
+# call the function
+
+x = 90
+y = 10
+sum(x,y)               # a and b actual arguments    
+    
+   # 100 
