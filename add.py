2763100093aa88cb1c069330bdef396eaf107dfc
bhumivegad <110536859+bhumivegad@users.noreply.github.com>
2023-03-14 17:44:47 +0530

    Add files via upload

---
+++ add.py
@@ -0,0 +1,12 @@
+# addition  user give a number
+
+def add(a,b):
+    return a+b
+i = int(input("enter number"))
+j = int(input("enter number"))
+
+print(add(i,j))
+
+#enter number354
+# enter number442
+# 796
